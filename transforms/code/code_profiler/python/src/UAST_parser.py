91ba054f7cdaf4ac704dcbaf7e72b82c1ece0f3a
aishwariyachakraborty <aishwariya.chakraborty@gmail.com>
2024-10-17 16:16:47 +0530

    partial changes for semantic
    Signed-off-by: aishwariyachakraborty <aishwariya.chakraborty@gmail.com>

--- transforms/code/code_profiler/python/src/UAST_parser.py
+++ transforms/code/code_profiler/python/src/UAST_parser.py
@@ -263,3 +263,31 @@ class UASTParser():
             return self.grammar[node_type]["keyword"] + " " + self.extracted        
         except Exception as e:
             print(e)
+
+def uast_read(jsonstring):
+    """
+    Reads an input json string into UAST class object
+    """
+    uast = UAST()
+    if jsonstring is not None and jsonstring != 'null':
+        uast.load_from_json_string(jsonstring)
+        return uast
+    return None
+
+def extract_ccr(uast):
+    """
+    Calculates the code to comment ratio given an UAST object as input
+    """
+    if uast is not None:
+        total_comment_loc = 0
+        for node_idx in uast.nodes:
+            node = uast.get_node(node_idx)
+            if node.node_type == 'uast_comment':
+                total_comment_loc += node.metadata.get("loc_original_code", 0)
+            elif node.node_type == 'uast_root':
+                loc_snippet = node.metadata.get("loc_snippet", 0)
+        if total_comment_loc > 0:
+            return loc_snippet / total_comment_loc
+        else:
+            return None 
+    return None
