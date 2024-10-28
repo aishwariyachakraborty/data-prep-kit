d99b7ddc30730fffe0373d8a263c337c949d5dbe
aishwariyachakraborty <aishwariya.chakraborty@gmail.com>
2024-10-28 15:49:27 +0530

    extract ccr func
    Signed-off-by: aishwariyachakraborty <aishwariya.chakraborty@gmail.com>

--- transforms/code/higher_order_syntactic_profiler/python/src/hosp_transform.py
+++ transforms/code/higher_order_syntactic_profiler/python/src/hosp_transform.py
@@ -40,6 +40,24 @@ def uast_read(jsonstring):
         return uast
     return None
 
+def extract_ccr(uast):
+    """
+    Calculates the code to comment ratio given an UAST object as input
+    """
+    if uast is not None:
+        total_comment_loc = 0
+        for node_idx in uast.nodes:
+            node = uast.get_node(node_idx)
+            if node.node_type == 'uast_comment':
+                total_comment_loc += node.metadata.get("loc_original_code", 0)
+            elif node.node_type == 'uast_root':
+                loc_snippet = node.metadata.get("loc_snippet", 0)
+        if total_comment_loc > 0:
+            return loc_snippet / total_comment_loc
+        else:
+            return None 
+    return None
+
 
 def generate_report(table: pa.Table, metrics_list):
     """
